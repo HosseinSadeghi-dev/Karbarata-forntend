025658467e3c914ed28d5ca9de798477b07c55eb
Ali Farabi <farabiali.af@gmail.com>
2020-08-16 15:07:49 +0430

    home page 3 first sectiin

--- src/app/modules/main/main-routing.module.ts
+++ src/app/modules/main/main-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './pages';
 
 
 const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  }
 ];
 
 @NgModule({
--- src/app/modules/main/main.module.ts
+++ src/app/modules/main/main.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { MainRoutingModule } from './main-routing.module';
 import {HomeComponent} from "./pages";
+import { SharedModule } from '@app/shared/shared.module';
 
 
 @NgModule({
@@ -11,7 +12,8 @@ import {HomeComponent} from "./pages";
   ],
   imports: [
     CommonModule,
-    MainRoutingModule
+    MainRoutingModule,
+    SharedModule
   ]
 })
 export class MainModule { }
