5d8fe675041d56a417fa937d0d0c09438daa68fd
hossein_sadeghi <hskhabbaz@gmail.com>
2020-10-27 12:43:52 +0330

    Tests

--- src/app/modules/main/pages/home/home.component.ts
+++ src/app/modules/main/pages/home/home.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
 
   public works:any[] = [
-    {name:'نیرو ساده', imgSrc:'assets/image/company.png' ,slug:"simple"},
+    {name:'نیرو ساده', imgSrc:'assets/image/engineer.svg' ,slug:"simple"},
     {name:'نیرو متخصص', imgSrc:'assets/image/company.png' ,slug:"master"},
     {name:'طراحی ، ساخت و ساز', imgSrc:'assets/image/company.png' ,slug:"construct"},
     {name:'خدمات شهرداری', imgSrc:'assets/image/company.png' ,slug:"municipality"},
