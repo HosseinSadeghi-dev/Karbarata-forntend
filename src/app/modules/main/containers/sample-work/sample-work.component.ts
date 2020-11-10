1cda107429d5e2fd381424c32b08c01aa7f1ef17
hossein_sadeghi <hskhabbaz@gmail.com>
2020-11-10 13:31:56 +0330

    fix homepage

--- src/app/modules/main/containers/sample-work/sample-work.component.ts
+++ src/app/modules/main/containers/sample-work/sample-work.component.ts
@@ -12,33 +12,75 @@ import { Component, OnInit } from '@angular/core';
 export class SampleWorkComponent implements OnInit {
 
   public samples = [
+    // {
+    //   title: 'تامین نیرو',
+    //   works:[
+    //     {id:0, imgSrc:'assets/image/Services/sakhtemon.png'},
+    //     {id:1, imgSrc:'assets/image/Services/sakhtemon.png'},
+    //     {id:2, imgSrc:'assets/image/Services/sakhtemon.png'},
+    //     {id:3, imgSrc:'assets/image/Services/sakhtemon.png'},
+    //   ]
+    // },
     {
-      title: 'ساختمان',
+      title: 'باز طراحی و بازسازی',
       works:[
-        {id:0, imgSrc:'assets/image/sakhtemon.png'},
-        {id:1, imgSrc:'assets/image/sakhtemon.png'},
-        {id:2, imgSrc:'assets/image/sakhtemon.png'},
-        {id:3, imgSrc:'assets/image/sakhtemon.png'},
+        {id:0, imgSrc:'assets/image/Services/Redesign/1.jpg'},
+        {id:1, imgSrc:'assets/image/Services/Redesign/2.jpg'},
+        {id:2, imgSrc:'assets/image/Services/Redesign/3.jpg'},
+        {id:3, imgSrc:'assets/image/Services/Redesign/4.jpg'},
+        {id:4, imgSrc:'assets/image/Services/Redesign/5.jpg'},
+        {id:5, imgSrc:'assets/image/Services/Redesign/6.jpg'},
+        {id:6, imgSrc:'assets/image/Services/Redesign/7.jpg'},
+        {id:7, imgSrc:'assets/image/Services/Redesign/8.jpg'},
+        {id:8, imgSrc:'assets/image/Services/Redesign/9.jpg'},
+        {id:9, imgSrc:'assets/image/Services/Redesign/10.jpg'},
+        {id:10, imgSrc:'assets/image/Services/Redesign/11.jpg'},
+        {id:11, imgSrc:'assets/image/Services/Redesign/12.jpg'},
       ]
     },
     {
-      title: 'نمونه دو',
+      title: 'طراحی و اجرا باغ - ویلا',
       works:[
-        {id:0, imgSrc:'assets/image/sakhtemon.png'},
-        {id:1, imgSrc:'assets/image/sakhtemon.png'},
-        {id:2, imgSrc:'assets/image/sakhtemon.png'},
-        {id:3, imgSrc:'assets/image/sakhtemon.png'},
+        {id:0, imgSrc:'assets/image/Services/Baq/1.jpg'},
+        {id:1, imgSrc:'assets/image/Services/Baq/2.jpg'},
+        {id:2, imgSrc:'assets/image/Services/Baq/3.jpg'},
+        {id:3, imgSrc:'assets/image/Services/Baq/4.jpg'},
+        {id:4, imgSrc:'assets/image/Services/Baq/5.jpg'},
+        {id:5, imgSrc:'assets/image/Services/Baq/6.jpg'},
+        {id:6, imgSrc:'assets/image/Services/Baq/7.jpg'},
+        {id:7, imgSrc:'assets/image/Services/Baq/8.jpg'},
+        {id:8, imgSrc:'assets/image/Services/Baq/9.jpg'},
+        {id:9, imgSrc:'assets/image/Services/Baq/10.jpg'},
       ]
     },
     {
-      title: 'نمونه سه',
+      title: 'طراحی و اجرا واحد مسکونی',
       works:[
-        {id:0, imgSrc:'assets/image/sakhtemon.png'},
-        {id:1, imgSrc:'assets/image/sakhtemon.png'},
-        {id:2, imgSrc:'assets/image/sakhtemon.png'},
-        {id:3, imgSrc:'assets/image/sakhtemon.png'},
+        {id:0, imgSrc:'assets/image/Services/Residential/1.jpg'},
+        {id:1, imgSrc:'assets/image/Services/Residential/2.jpg'},
+        {id:2, imgSrc:'assets/image/Services/Residential/3.jpg'},
+        {id:3, imgSrc:'assets/image/Services/Residential/4.jpg'},
+        {id:4, imgSrc:'assets/image/Services/Residential/5.jpg'},
       ]
-    }
+    },
+    {
+      title: 'طراحی و اجرا واحد های تجاری و دفتر کار',
+      works:[
+        {id:0, imgSrc:'assets/image/Services/Office/1.jpg'},
+        {id:1, imgSrc:'assets/image/Services/Office/2.jpg'},
+        {id:2, imgSrc:'assets/image/Services/Office/3.jpg'},
+        {id:3, imgSrc:'assets/image/Services/Office/4.jpg'},
+        {id:4, imgSrc:'assets/image/Services/Office/5.jpg'},
+        {id:5, imgSrc:'assets/image/Services/Office/6.jpg'},
+        {id:6, imgSrc:'assets/image/Services/Office/7.webp'},
+        {id:7, imgSrc:'assets/image/Services/Office/8.webp'},
+        {id:8, imgSrc:'assets/image/Services/Office/9.jpg'},
+        {id:9, imgSrc:'assets/image/Services/Office/10.jpg'},
+        {id:10, imgSrc:'assets/image/Services/Office/11.jpg'},
+        {id:11, imgSrc:'assets/image/Services/Office/12.jpg'},
+        {id:11, imgSrc:'assets/image/Services/Office/13.webp'},
+      ]
+    },
   ];
 
   constructor() { }
--- src/app/modules/main/pages/home/home.component.ts
+++ src/app/modules/main/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -8,16 +8,15 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
 
   public works:any[] = [
-    {name:'نیرو ساده', imgSrc:'assets/image/engineer.svg' ,slug:"simple"},
-    {name:'نیرو متخصص', imgSrc:'assets/image/company.png' ,slug:"master"},
-    {name:'طراحی ، ساخت و ساز', imgSrc:'assets/image/company.png' ,slug:"construct"},
-    {name:'خدمات شهرداری', imgSrc:'assets/image/company.png' ,slug:"municipality"},
-    {name:'مشاوره و استعلام قیمت', imgSrc:'assets/image/price-advise.jpg' ,slug:"price-advice"}
+    {name:'نیرو ساده', imgSrc:'assets/image/simple-worker.png' ,slug:"simple"},
+    {name:'نیرو متخصص', imgSrc:'assets/image/expert-worker.png' ,slug:"master"},
+    {name:'طراحی ، ساخت و ساز', imgSrc:'assets/image/construct-worker.png' ,slug:"construct"},
+    {name:'خدمات شهرداری', imgSrc:'assets/image/municipal-worker.png' ,slug:"municipality"},
+    {name:'مشاوره و استعلام قیمت', imgSrc:'assets/image/price-advise-worker.png' ,slug:"price-advice"}
   ]
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
 }
